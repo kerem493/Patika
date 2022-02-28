94dbc197363851065d9f7b334115d8ed5ab22060
Kerem KORKMAZ <78110525+kerem493@users.noreply.github.com>
2022-02-28 19:53:24 +0300

    if-else

---
+++ if-else/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time <= 18)
+            {
+                System.Console.WriteLine("İyi günler.");
+            }
+            else if (18 < time && time <= 22)
+            {
+                System.Console.WriteLine("İyi akşamlar.");
+            }
+            else
+            {
+                System.Console.WriteLine("İyi geceler.");
+            }
+
+            string sonuc = 18<time && time<=22 ? "İyi akşamlar" : time <=18 ? "İyi günler." : "İyi geceler."; 
+            System.Console.WriteLine(sonuc);
+
+
+
+
+            Console.Read();
+        }
+    }
+}
